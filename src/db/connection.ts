4354e9ed13415f81bd21a163d712d9c1f33e8062
caioviniciusml <devcaioviniciusml@gmail.com>
2025-07-08 08:57:47 -0300

    feat: add rooms listing

---
+++ src/db/connection.ts
@@ -0,0 +1,10 @@
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import { env } from '../env.ts'
+import { schema } from './schema/index.ts'
+
+export const postgresClient = postgres(env.DB_URL)
+export const db = drizzle(postgresClient, {
+  schema,
+  casing: 'snake_case',
+})
